6318dfe033bfc19582c82564c47d06ff15bc844c
Yuri Tayner <91205403+YuriTayner@users.noreply.github.com>
2026-03-02 20:13:43 -0300

    V0.0.7 Inicio da estruturação do login

--- src/main/java/com/sgc/app/dto/UsuarioLoginDTO.java
+++ src/main/java/com/sgc/app/dto/UsuarioLoginDTO.java
@@ -1,5 +1,32 @@
 package com.sgc.app.dto;
 
 public class UsuarioLoginDTO {
+    private String email;
+    private String senha;
 
+
+    public UsuarioLoginDTO(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+
+
+    public UsuarioLoginDTO() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
